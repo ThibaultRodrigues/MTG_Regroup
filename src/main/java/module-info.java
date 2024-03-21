bcec279f5acf42765f188768d664a4782e873c2a
Rodrigues-seabra <thibaultrodrigguesseabra@gmail.com>
2024-03-21 11:49:08 +0100

    Je sais toujours pas

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module siothibault.magic_regroupment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens siothibault.magic_regroupment to javafx.fxml;
+    exports siothibault.magic_regroupment;
+}
---
+++ src/main/java/siothibault/magic_regroupment/Carte.java
@@ -0,0 +1,7 @@
+package siothibault.magic_regroupment;
+
+public class Carte {
+    private String name;
+    private String color;
+    private String img;
+}

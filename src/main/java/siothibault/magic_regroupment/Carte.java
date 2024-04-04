83a163e69dd9262b4fd927491bf1ae440d970044
Rodrigues-seabra <thibaultrodrigguesseabra@gmail.com>
2024-04-04 09:19:35 +0200

    Je sais toujours pas

--- src/main/java/siothibault/magic_regroupment/Carte.java
+++ src/main/java/siothibault/magic_regroupment/Carte.java
@@ -4,4 +4,16 @@ public class Carte {
     private String name;
     private String color;
     private String img;
+    private int force;
+    private int defense;
+
+    public String getName() {
+        return this.name;
+    }
+    public int getDefense() {
+        return this.defense;
+    }
+    public int getForce(){
+        return this.force;
+    }
 }

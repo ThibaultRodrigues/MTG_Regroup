031e98dd8b50057d432d06b480095a6b9b0b5365
Thibault Rodrigues-seabra <titi42464@gmail.com>
2024-04-27 15:07:35 +0200

    Petit Test

--- src/main/java/siothibault/magic_regroupment/HelloController.java
+++ src/main/java/siothibault/magic_regroupment/HelloController.java
@@ -122,6 +122,7 @@ public class HelloController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        changeAP(MenuChoix);
 
     }
 
